ab0024de6ef9cffaddcce0bd7f63dac73aadf4f5
Azigaming404 <120331083+Azigaming404@users.noreply.github.com>
2023-03-08 11:36:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 import openai
-openai.api_key = "isi-api-bot"
+openai.api_key = "isi-api-bot-openAI"
 api = '5757441699:AAG3Sm8DDZne0YduA9t-IkUhPM23T5Hz_Po'
 bot = telebot.TeleBot(api)
 
