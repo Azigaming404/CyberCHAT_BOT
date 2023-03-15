d8c99d92f5afbd7827968d333ee32191051709f2
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-15 22:41:11 +0700

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+blue='\033[0;34m'
+ungu='\033[0;35m'
+Green="\033[32m"
+Red="\033[31m"
+WhiteB="\e[5;37m"
+BlueCyan="\e[5;36m"
+Green_background="\033[42;37m"
+Red_background="\033[41;37m"
+Suffix="\033[0m"
 clear
 sleep 1
 echo "=================================================="
-figlet -f 3d "CyberCHAT BOT" | lolcat
-echo "=================================================="
+echo -e "${Red}"
+figlet -f 3d "CyberCHAT BOT"
+echo -e "${WhiteB}=================================================="
 echo "IP VPS   : 167.71.199.138"
 echo "Timezone : lndonesia/Jakarta (GMT+7)"
 echo "Role     : ChatGPT OpenAI"
