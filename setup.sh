fe4de0d752ff799c184efdc58eb03b398a69b4df
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-16 00:14:29 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -57,7 +57,7 @@ then
 clear
 echo "Cheking update" | lolcat
 sleep 2
-bash update.sh
+wget https://raw.githubusercontent.com/Azigaming404/CyberCHAT_BOT/main/banner.sh
 fi
 
 if [ $bro = 2 ] || [ $bro = 2 ]
