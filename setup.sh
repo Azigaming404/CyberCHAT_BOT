4548c50011938cd3eeff96777b570d40679eadb7
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-15 21:56:05 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+blue='\033[0;34m'
 ungu='\033[0;35m'
+Green="\033[32m"
+Red="\033[31m"
+WhiteB="\e[5;37m"
+BlueCyan="\e[5;36m"
+Green_background="\033[42;37m"
+Red_background="\033[41;37m"
+Suffix="\033[0m"
 bash banner.sh
 echo -e "${ungu}====================================================" 
 date | lolcat
