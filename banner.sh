a0ed3f2582ef2ca59a38e3e408274bcc1aa55805
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-15 22:43:50 +0700

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -23,6 +23,6 @@ echo "Timezone : lndonesia/Jakarta (GMT+7)"
 echo "Role     : ChatGPT OpenAI"
 echo "OS       : Ubuntu 20.04 LTS"
 echo "Version  : V.3.0.0 Lates"
-echo "==================================================" | lolcat
+echo "=================================================="
 echo
 echo
