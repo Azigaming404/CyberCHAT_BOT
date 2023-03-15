ff4731a15fbefc6ce858bca90dda70c6cc11335b
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-15 21:49:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 ungu='\033[0;35m'
 bash banner.sh
-echo -e "{ungu}====================================================" 
+echo -e "${ungu}====================================================" 
 date | lolcat
-echo -e "{ungu}____________________________________________________"
+echo -e "${ungu}____________________________________________________"
 echo "                 script by: A7i" | lolcat
-echo -e "{ungu}____________________________________________________"
+echo -e "${ungu}____________________________________________________"
 echo "                      menu" | lolcat
 echo "1.start bot telegram"
 echo "2.start bot whatsapp" 
 echo "3.update bot" 
 echo "4.fix update"
 echo 
-echo -e "{ungu}___________________________________________________" 
+echo -e "${ungu}___________________________________________________" 
 echo "Client Name: AZI GANTENG"
 echo "Region : Indonesia" 
 echo "Status : Pemilik Script"
 echo "my love : deby"
-echo -e "{ungu}___________________________________________________"
+echo -e "${ungu}___________________________________________________"
 read -p    "Pilih Nomor :" bro 
 
 if [ $bro = 1 ] || [ $bro = 1 ]
