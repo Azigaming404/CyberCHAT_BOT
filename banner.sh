d94eba2239766372a4e7f53d51b81eb9d12472da
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-03-15 20:42:45 +0700

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+Clear
+echo "===============================================" | lolcat
+echo "IP VPS   : 167.71.199.138"
+echo "Timezone : lndonesia/Jakarta (GMT+7)
+echo "Role     : ChatGPT OpenAI"
+echo "OS       : Ubuntu 20.04 LTS
+echo "Version  : V.3.0.0 Lates
+echo "================================================" | lolcat
+echo
+echo
